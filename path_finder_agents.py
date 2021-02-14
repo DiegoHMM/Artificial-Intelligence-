b5c5fd67bc928f53ce9175af70fed3efaf9280a1
Diego Henrique Marques Matos <diego.matos@aluno.ufop.edu.br>
2021-02-14 17:30:00 -0300

    feat/a_star

--- path_finder_agents.py
+++ path_finder_agents.py
@@ -173,6 +173,7 @@ class BBAgent(Agent):
         # Select a path from the frontier
         path = self.frontier.pop(0)
         cost = self.cost.pop(0)
+        
 
         #heuristica
         if cost + distance.euclidean(path[-1],self.percepts['target']) <self.bound:
@@ -375,5 +376,88 @@ class BFSAgent(Agent):
         print(self.percepts['current_position'])
 
     
+class AStarAgent(Agent):
+    """
+    This class implements an agent that explores the environmente randomly
+    until it reaches the target
+    """
+
+    def __init__(self, env):
+        """Connects to the next available port.
+
+        Args: 
+            env: A reference to an environment.
+
+        """
+
+        # Make a connection to the environment using the superclass constructor
+        Agent.__init__(self,env)
+        
+        # Get initial percepts
+        self.percepts = env.initial_percepts()
+        
+        # Initializes the frontier with the initial postion 
+        self.frontier = [[self.percepts['current_position']]]
+        
+        # Initializes list of visited nodes for multiple path prunning
+        self.visited = []
+        self.cost = [0]
+
+    def act(self):
+        """Implements the agent action
+        """
+
+        path = self.frontier.pop() #retirei o caminho da fronteira 
+        cost = self.cost.pop(0)
+        path_dict = {cost + distance.euclidean(path[-1],self.percepts['target']): path[-1]}
+
+        for p in self.frontier:
+            path_dict[cost + distance.euclidean(p[-1],self.percepts['target'])] = p
+
+        path_dict = collections.OrderedDict(sorted(path_dict.items())) #Ordena meu dict
+
+        if len(path) == 1:
+            opa = True
+            menor_path = [list(path_dict.values())[0]]
+        else :
+            opa=False
+            menor_path = list(path_dict.values())[0]
+
+        # Visit the last node in the path
+        action = {'visit_position': menor_path[-1], 'path': menor_path} 
+        # The agente sends a position and the full path to the environment, the environment can plot the path in the room 
+        self.percepts = self.env.signal(action)
+            
+        viable_neighbors =  self.percepts['neighbors']
+
+        # If the agent is not stuck
+        if viable_neighbors:
+            insertFrontier = True
+            for n in viable_neighbors:
+                for cycle in path:
+                    if(n == cycle).all():
+                        insertFrontier = False
+                        break
+
+                    for aux in self.visited:
+                        if (n == aux).all():
+                            insertFrontier = False
+                            break
+
+                    if insertFrontier:
+                        self.frontier = [path + [n]] + self.frontier
+                        self.cost = [cost + distance.euclidean(path[-1],n)] + self.cost
+                # Append neighbor to the path and add it to the frontier
+                
+            
+
+    def run(self):
+        """Keeps the agent acting until it finds the target
+        """
+
+        # Run agent
+        while (self.percepts['current_position'] != self.percepts['target']).any() and self.frontier:
+            self.act()
+        print(self.percepts['current_position'])
 
 
